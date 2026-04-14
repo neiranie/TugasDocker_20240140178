f99262e864be7cb974acdf7c9dc6ef63d6316383
neiranie <email_anda@contoh.com>
2026-04-14 14:16:21 +0700

    Added User

--- deploy/src/main/java/com/tugas/deploy/controller/UserController.java
+++ deploy/src/main/java/com/tugas/deploy/controller/UserController.java
@@ -1,15 +1,21 @@
-package com.praktikum5.deploy.controller;
+package com.tugas.deploy.controller;
 
+import com.tugas.deploy.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
     private final String USERNAME = "admin";
     private final String PASSWORD = "20240140178";
+
+    private List<User> users = new ArrayList<>();
 
     @GetMapping("/")
     public String loginpage() {
@@ -21,15 +27,32 @@ public class UserController {
                         @RequestParam String password,
                         Model model) {
         if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            model.addAttribute("users", users);
             return "home";
         } else {
-            model.addAttribute("error",
-                    "Invalid username or password");
+            model.addAttribute("error", "Invalid username or password");
             return "login";
         }
     }
+
     @GetMapping("/home")
-    public String homepage() {
+    public String homepage(Model model) {
+        model.addAttribute("users", users);
+        return "home";
+    }
+
+    @GetMapping("/form")
+    public String formpage() {
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String submitForm(@RequestParam String nama,
+                             @RequestParam String nim,
+                             @RequestParam String jenisKelamin,
+                             Model model) {
+        users.add(new User(nama, nim, jenisKelamin));
+        model.addAttribute("users", users);
         return "home";
     }
 }
